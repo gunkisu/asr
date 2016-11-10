1f73f0425202405ee4487c1eb01c8ae886f36473
Inchul Song <icsong@gmail.com>
2016-11-10 17:09:47 -0500

    Code for adding features

--- data/wsj/create_add_targets.py
+++ data/wsj/create_add_targets.py
@@ -1,8 +1,12 @@
 # IPython log file
-
-import h5py
-f =h5py.File('/u/songinch/song/data/speech/wsj_fbank123.h5', 'a')
+import kaldi_io
 import numpy
+import h5py
+
+
+f = h5py.File('/u/songinch/song/data/speech/wsj_fbank123.h5', 'a')
+
+# Create datasets
 features = f.create_dataset('features', (38230,), dtype=h5py.special_dtype(vlen=numpy.float32), maxshape=(None,))
 features.dims[0].label = 'batch'
 targets = f.create_dataset('targets', (37394,), dtype=h5py.special_dtype(vlen=numpy.int32), maxshape=(None,))
@@ -22,11 +26,18 @@ targets.dims[0].attach_scale(targets_shapes)
 targets.dims.create_scale(targets_shapes_labels, 'shape_labels')
 targets.dims[0].attach_scale(targets_shapes_labels)
 
+# Add target information
 tmp = [l.strip().split(None, 1) for l in open('exp/hyperud/all_targets.txt')]
 tmp_uttid = [(a, b.split()) for a, b in tmp]
     
 for num_utt, (uttid, value) in enumerate(tmp_uttid):
     targets_shapes[num_utt,:] = len(value) 
     targets[num_utt] = numpy.asarray(value).ravel()
+
+# Add features
+reader = kaldi_io.SequentialBaseFloatMatrixReader('scp:data/test_eval92/feats.scp')
+for num_utts, (uttid, value) in enumerate(reader):
+    features_shapes[num_utts,:] =featuresvalue.shape
+    features[num_utts] = value.ravel()
     
 f.close()
