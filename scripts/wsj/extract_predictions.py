f228a7cc2570dabfaa23eca63b431d16df6de418
Inchul Song <icsong@gmail.com>
2016-11-10 20:39:58 -0500

    Extract and stores outputs of the network for decoding

---
+++ scripts/wsj/extract_predictions.py
@@ -0,0 +1,106 @@
+from argparse import ArgumentParser
+import numpy, theano, lasagne, pickle
+from theano import tensor as T
+from collections import OrderedDict
+from models.baseline import deep_bidir_lstm_model
+from lasagne.layers import get_output, get_all_params
+
+from fuel.datasets.hdf5 import H5PYDataset
+from fuel.streams import DataStream
+from data.schemes import SequentialShuffledScheme
+from fuel.transformers import Padding
+import kaldi_io
+
+from libs.lasagne.utils import get_model_param_values, get_update_params_values
+
+import sys
+
+floatX = theano.config.floatX
+
+def get_datastream(path, which_set='test_eval92', batch_size=1):
+    wsj_dataset = H5PYDataset(path, which_sets=(which_set, ))
+    shuffle_rng = numpy.random.RandomState(123)
+    iterator_scheme = SequentialShuffledScheme(num_examples=wsj_dataset.num_examples,
+                                               batch_size=batch_size,
+                                               rng=shuffle_rng)
+    base_stream = DataStream(dataset=wsj_dataset,
+                             iteration_scheme=iterator_scheme)
+    padded_stream = Padding(data_stream=base_stream)
+
+    return padded_stream
+
+def build_network(input_data,
+                  input_mask,
+                  num_inputs=123,
+                  num_units_list=(128, 128, 128),
+                  num_outputs=63,
+                  dropout_ratio=0.2,
+                  use_layer_norm=True,
+                  learn_init=True,
+                  grad_clipping=0.0):
+    network = deep_bidir_lstm_model(input_var=input_data,
+                                    mask_var=input_mask,
+                                    num_inputs=num_inputs,
+                                    num_units_list=num_units_list,
+                                    num_outputs=num_outputs,
+                                    dropout_ratio=dropout_ratio,
+                                    use_layer_norm=use_layer_norm,
+                                    learn_init=learn_init,
+                                    grad_clipping=grad_clipping)
+    return network
+
+def main(options):
+    input_data = T.ftensor3('input_data')
+    input_mask = T.fmatrix('input_mask')
+    target_data = T.imatrix('target_data')
+    target_mask = T.fmatrix('target_mask')
+
+    network = build_network(input_data=input_data,
+                            input_mask=input_mask,
+                            num_inputs=options['num_inputs'],
+                            num_units_list=options['num_units_list'],
+                            num_outputs=options['num_outputs'],
+                            dropout_ratio=0.2,
+                            use_layer_norm=True,
+                            learn_init=True,
+                            grad_clipping=0.0)
+    network_params = get_all_params(network, trainable=True)
+
+    if not options['load_params']:
+        print 'Must specfiy network to load'
+        sys.exit(1)
+
+    ff_fn = ff(input_data=input_data,
+                                       input_mask=input_mask,
+                                       target_data=target_data,
+                                       target_mask=target_mask,
+                                       network=network)
+
+    test_stream = get_datastream(options['data_path'], 'test_eval92', options['batch_size']) 
+
+
+    for example in test_stream.get_epoch_iterator():
+        net_output = ff_fn(*example)
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    # TODO: parser
+
+    options = OrderedDict()
+    options['num_units_list'] =  (250, 250, 250, 250, 250)
+    options['num_inputs'] = 123
+    options['num_outputs'] = 63
+    options['dropout_ratio'] = 0.2
+    options['use_layer_norm'] = True
+
+    options['data_path'] = '/home/songinch/data/speech/timit_fbank_framewise.h5'
+    options['save_path'] = '/home/songinch/data/speech/'
+    options['load_params'] = '/path/to/model' 
+
+    main(options)
+
+
+
+
+
+
