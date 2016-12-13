c394fad585e6a350d91ecfcb6878b16a6b181ea8
Inchul Song <icsong@gmail.com>
2016-12-13 15:06:26 -0500

    plot graphs over FERs or Prediction Costs

---
+++ libs/plot_fer.py
@@ -0,0 +1,24 @@
+# IPython log file
+import argparse
+import matplotlib.pyplot as plt
+import numpy
+
+def uniq(seq):
+    checked = []
+    for e in seq:
+        if e not in checked:
+            checked.append(e)
+    return checked
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('logfile', help='log file')
+args = parser.parse_args()
+print args
+
+with open(args.logfile) as f:
+    lines_to_plot = [l for l in f if 'FER' in l]
+    lines_to_plot = uniq(lines_to_plot)
+    nums_to_plot = [float(l.strip().split()[-1]) for l in lines_to_plot] 
+    plt.plot(nums_to_plot)
+    plt.show()
+
