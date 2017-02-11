6b12cb65b7c561b96a63e749c7d118ef7ed7e46f
Inchul Song <icsong@gmail.com>
2017-02-11 10:48:24 -0500

    Removing the commment

--- model_find.py
+++ model_find.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python
-'''Find the log file of a job launched by smart-dispatch 
-based on the job id and log file extension.
-It can be used together with other linux commands such as tail and grep
-to monitor your jobs'''
-
 from __future__ import print_function
 
 from libs.utils import log_find
 import argparse
-
 
 def param_extract(param, opt_line):
     items = opt_line.split(',')
