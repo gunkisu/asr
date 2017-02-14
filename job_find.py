9b938f898b2df24694a81226aa3c767d8e96e345
Inchul Song <icsong@gmail.com>
2017-02-14 14:48:25 -0500

    Adding job_find_all.py for finding all jobs launched by smart-dispatch

--- job_find.py
+++ job_find.py
@@ -2,36 +2,7 @@
 from __future__ import print_function
 
 import subprocess
-import argparse
-import sys
 import glob
-
-def extract_jobid(jobid_file):
-    jobid = ''
-    with open(jobid_file) as f:
-        for l in f:
-            if 'helios' in l:
-                jobid = l.split('.')[0]
-
-    # There can be multiple job ids because of resumption.
-    # The last job id will be returned.
-
-    return jobid
-
-def extract_all_jobids(log_dir='SMART_DISPATCH_LOGS'):
-    jobid_files = glob.glob('{}/*/jobs_id.txt'.format(log_dir))
-    return [ extract_jobid(jf) for jf in jobid_files ]
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--all', action='store_true')
-args = parser.parse_args()
-
-
-if args.all:
-    jobids = extract_all_jobids()
-    for ji in sorted(jobids): 
-        if ji: print(ji)
-    sys.exit(0) 
 
 p = subprocess.Popen("qstat", shell=True, stdout=subprocess.PIPE)
 out = p.stdout.read()
