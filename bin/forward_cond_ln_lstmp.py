30b077546c33604473005ce9328ec24a38b65611
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2017-03-14 05:26:10 +0900

    hyper net layer

--- bin/forward_cond_ln_lstmp.py
+++ bin/forward_cond_ln_lstmp.py
@@ -118,6 +118,10 @@ if __name__ == '__main__':
         print('Writing outputs...', file=sys.stderr)
         for out_idx, (output, uttid) in enumerate(zip(net_output[0], uttid_batch[0])):
             valid_len = feat_lens[out_idx]
+            print(uttid)
+            raw_input()
+            print(numpy.log(output[:valid_len]))
             writer.write(uttid.encode('ascii'), numpy.log(output[:valid_len]))
+            raw_input()
 
     writer.close()
