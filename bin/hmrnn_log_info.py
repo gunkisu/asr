d5779e0f195d6332c725ed6744bf78f4ac84d7a2
Inchul Song <icsong@gmail.com>
2017-05-01 15:52:12 -0400

    Getting the log file as an argument

--- bin/hmrnn_log_info.py
+++ bin/hmrnn_log_info.py
@@ -1,12 +1,18 @@
 # IPython log file
 from __future__ import print_function
 
+import argparse
+
 def parse_line(line):   
     value_part = line.split(':')[1].strip()
     value_part = value_part.split(' ')[0]
     return value_part
 
-lines = open('train_log').readlines()
+parser = argparse.ArgumentParser()
+parser.add_argument('log_file')
+args = parser.parse_args()
+
+lines = open(args.log_file).readlines()
 for l in lines: 
     if 'Model Name' in l:
         print(parse_line(l), end=' ')
