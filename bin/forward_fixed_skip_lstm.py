10fd97d174bbcd3c894305adc826d8eaeeaebcbd
Inchul Song <icsong@gmail.com>
2017-06-12 16:50:40 -0400

    Big fix (still it seems that the weights are not loaded)

--- bin/forward_fixed_skip_lstm.py
+++ bin/forward_fixed_skip_lstm.py
@@ -13,7 +13,7 @@ from skiprnn.mixer import save_npz2
 
 from collections import namedtuple, OrderedDict
 
-from data.fuel_utils import create_ivector_datastream, get_uttid_stream
+from data.fuel_utils import create_ivector_test_datastream, get_uttid_stream
 from libs.utils import sync_data, skip_frames_fixed, StopWatch
 
 from itertools import islice
@@ -22,23 +22,18 @@ import kaldi_io
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
-flags.DEFINE_float('learning-rate', 0.002, 'Initial learning rate')
 flags.DEFINE_integer('batch-size', 64, 'Size of mini-batch')
-flags.DEFINE_integer('min-after-cache', 1024, 'Size of mini-batch')
 flags.DEFINE_integer('n-input', 123, 'Number of RNN hidden units')
 flags.DEFINE_integer('n-hidden', 1024, 'Number of RNN hidden units')
 flags.DEFINE_integer('n-class', 3436, 'Number of target symbols')
-flags.DEFINE_integer('base-seed', 20170309, 'Base random seed') 
-flags.DEFINE_integer('add-seed', 0, 'Add this amount to the base random seed')
 flags.DEFINE_string('device', 'gpu', 'Simply set either `cpu` or `gpu`')
-flags.DEFINE_string('log-dir', 'skip_lstm_wsj', 'Directory path to files')
 flags.DEFINE_boolean('no-copy', True, '') # test set is typically small
-flags.DEFINE_boolean('no-length-sort', False, '')
 flags.DEFINE_string('tmpdir', '/Tmp/songinch/data/speech', '')
 flags.DEFINE_string('data-path', '/u/songinch/song/data/speech/wsj_fbank123.h5', '')
 flags.DEFINE_string('dataset', 'test_dev93', '')
 flags.DEFINE_integer('n-skip', 1, 'Number of frames to skip')
 flags.DEFINE_string('wxfilename', 'ark:-', '')
+flags.DEFINE_string('metafile', 'best_model.ckpt-1000.meta', '')
 
 TrainGraph = namedtuple('TrainGraph', 'ml_cost seq_x_data seq_x_mask seq_y_data init_state pred_idx seq_label_probs')
 
@@ -95,12 +90,7 @@ def main(_):
 
   tf.get_variable_scope()._reuse = None
 
-  _seed = args.base_seed + args.add_seed
-  tf.set_random_seed(_seed)
-  np.random.seed(_seed)
-
-  meta_filename, = glob.glob(os.path.join(args.log_dir, '*best*.meta'))
-  meta_file = os.path.basename(meta_filename)
+  meta_file = args.metafile
   model_file = meta_file[:-5]
 
   tg = build_graph(args)
@@ -108,9 +98,7 @@ def main(_):
   global_step = tf.Variable(0, trainable=False, name="global_step")
 
   sync_data(args)
-  test_set = create_ivector_datastream(path=args.data_path, which_set=args.dataset, 
-    batch_size=args.batch_size, min_after_cache=args.min_after_cache, 
-    length_sort=not args.no_length_sort)
+  test_set = create_ivector_test_datastream(args.data_path, args.dataset, args.batch_size)
 
   uttid_stream = get_uttid_stream(args.data_path, args.dataset, args.batch_size) 
 
@@ -118,30 +106,29 @@ def main(_):
 
   with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-        
+    
     print('Loading model...', file=sys.stderr)
-    save_op = tf.train.import_meta_graph(os.path.join(args.log_dir, meta_file))
-    save_op.restore(sess, os.path.join(args.log_dir, model_file))
+    save_op = tf.train.import_meta_graph(meta_file)
+    save_op.restore(sess, model_file)
 
     writer = kaldi_io.BaseFloatMatrixWriter(args.wxfilename)
 
     print('Computing label probs...', file=sys.stderr)
 
     for bidx, (batch, uttid_batch) in enumerate(zip(test_set.get_epoch_iterator(), uttid_stream.get_epoch_iterator())):
-      orig_x, orig_x_mask, _, _, orig_y, _ = batch
+      orig_x, orig_x_mask, _, _ = batch
       uttid_batch, = uttid_batch
 
       feat_lens = orig_x_mask.sum(axis=1)
 
-      sub_batch, = list(skip_frames_fixed([orig_x, orig_x_mask, orig_y], args.n_skip+1, return_first=True))
-      x, x_mask, y = sub_batch
+      sub_batch, = list(skip_frames_fixed([orig_x, orig_x_mask], args.n_skip+1, return_first=True))
+      x, x_mask = sub_batch
       n_batch, n_seq, _ = x.shape
 
       feed_states = initial_states(n_batch, args.n_hidden)
 
       seq_label_probs, = sess.run([tg.seq_label_probs],
-        feed_dict={tg.seq_x_data: x, tg.seq_x_mask: x_mask,
-        tg.seq_y_data: y, tg.init_state: feed_states})
+        feed_dict={tg.seq_x_data: x, tg.seq_x_mask: x_mask, tg.init_state: feed_states})
 
       seq_label_probs = seq_label_probs.reshape([n_batch, n_seq, -1])
       seq_label_probs = seq_label_probs.repeat(args.n_skip+1, axis=1)
