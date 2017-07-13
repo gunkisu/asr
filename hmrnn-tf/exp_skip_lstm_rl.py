d62d10907e90987dc8a471ff377b9de3e3854f1e
kim <kim.ts.kr@gmail.com>
2017-07-13 21:06:01 +0100

    fix

--- hmrnn-tf/exp_skip_lstm_rl.py
+++ hmrnn-tf/exp_skip_lstm_rl.py
@@ -282,10 +282,6 @@ def expand_pred_idx(seq_skip_1hot,
 
             expand_output[i, start_idx:end_idx] = p
             start_idx = end_idx
-        print(skip_step[:10])
-        print(prd_idx[:10])
-        print(expand_output[i, :50])
-        raw_input()
 
     return expand_output
 
