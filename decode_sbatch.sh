f45a295607bee402bc5313fe42eaea0befbc7c56
Inchul Song <icsong@gmail.com>
2017-07-28 11:53:56 -0400

    Adding skip lstms trained with supervised learning

--- decode_sbatch.sh
+++ decode_sbatch.sh
@@ -6,9 +6,22 @@ cd ~/song/kaldi/egs/wsj/s5
 source ./path.sh
 
 # skip lstm
-#mkdir exp/hyperud/decode_bd_tgpr_dev93_skip_lstm
-#python -u ~/song/asr/bin/forward_skip_lstm.py --batch-size 4 --metafile ~/song/asr/skip_lstm/best_model.ckpt-2340.meta | latgen-faster-mapped --min-active=200 --max-active=7000 --max-mem=50000000 --beam=13.0 --lattice-beam=8.0 --acoustic-scale=0.1 --word-symbol-table=exp/tri4b/graph_bd_tgpr/words.txt exp/hyperud/final.mdl exp/tri4b/graph_bd_tgpr/HCLG.fst ark:- "ark:|gzip -c > exp/hyperud/decode_bd_tgpr_dev93_skip_lstm/lat.1.gz"
+#dir=exp/hyperud/decode_bd_tgpr_dev93_skip_lstm_a2
+#model=~/song/asr/skip_lstm_a2/best_model.ckpt-32732.meta
+#forward=~/song/asr/bin/forward_skip_lstm.py 
 
-# fixed lstm
-mkdir exp/hyperud/decode_bd_tgpr_dev93_fixed_skip2
-python -u ~/song/asr/bin/forward_fixed_skip_lstm.py --batch-size 4 --metafile ~/song/asr/fixed_skip_lstm_wsj_skip2/best_model.ckpt-18698.meta | latgen-faster-mapped --min-active=200 --max-active=7000 --max-mem=50000000 --beam=13.0 --lattice-beam=8.0 --acoustic-scale=0.1 --word-symbol-table=exp/tri4b/graph_bd_tgpr/words.txt exp/hyperud/final.mdl exp/tri4b/graph_bd_tgpr/HCLG.fst ark:- "ark:|gzip -c > exp/hyperud/decode_bd_tgpr_dev93_fixed_skip2/lat.1.gz"
+# fixed skip lstm
+#dir=exp/hyperud/decode_bd_tgpr_dev93_fixed_skip_s1
+#model=~/song/asr/fixed_skip_s1/best_model.ckpt-21042.meta
+#forward=~/song/asr/bin/forward_fixed_skip_lstm.py 
+
+# skip lstm supervised
+dir=exp/hyperud/decode_bd_tgpr_dev93_skip_lstm_supervised_a3
+model=~/song/asr/skip_lstm_supervised_a3/best_model.ckpt-23380.meta
+forward=~/song/asr/bin/forward_skip_lstm_supervised.py 
+
+n_batch=1
+
+mkdir $dir
+python -u $forward --n-batch $n_batch --metafile $model | latgen-faster-mapped --min-active=200 --max-active=7000 --max-mem=50000000 --beam=13.0 --lattice-beam=8.0 --acoustic-scale=0.1 --word-symbol-table=exp/tri4b/graph_bd_tgpr/words.txt exp/hyperud/final.mdl exp/tri4b/graph_bd_tgpr/HCLG.fst ark:- "ark:|gzip -c > $dir/lat.1.gz"
+
