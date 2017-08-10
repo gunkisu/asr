04c320072c5486d4f1326782474a62e3cdb39d0e
Inchul Song <icsong@gmail.com>
2017-08-10 09:56:16 -0400

    Reflecting name changes

--- decode.sh
+++ decode.sh
@@ -6,19 +6,20 @@ cd ~/song/kaldi/egs/wsj/s5
 source ./path.sh
 
 # skiprnn trained by reinforce
-dir=exp/hyperud/decode_bd_tgpr_dev93_skiprnn_ri_a6_y_e32
-model=~/song/asr/skiprnn_ri_a6_y_e32/best_model.ckpt-7014.meta
-forward=~/song/asr/bin/forward_skiprnn_ri.py
+#dir=exp/hyperud/decode_bd_tgpr_dev93_skiprnn_ri_a6_y_e32
+#model=~/song/asr/skiprnn_ri_a6_y_e32/best_model.ckpt-11690.meta
+#forward=~/song/asr/bin/forward_skiprnn_ri.py
 
 # skiprnn with subsampling
-dir=exp/hyperud/decode_bd_tgpr_dev93_skiprnn_subsample_s0
-model=~/song/asr/skiprnn_subsample_s0_lr0.01/best_model.ckpt-11690.meta
-forward=~/song/asr/bin/forward_skiprnn_subsample.py
+#dir=exp/hyperud/decode_bd_tgpr_dev93_skiprnn_subsample_s1
+#model=~/song/asr/skiprnn_subsample_s1/best_model.ckpt-7014.meta
+#forward=~/song/asr/bin/forward_skiprnn_subsample.py
 
-# skip lstm supervised
-#dir=exp/hyperud/decode_bd_tgpr_dev93_skip_lstm_supervised_a7
-#model=~/song/asr/skip_lstm_supervised_a7/best_model.ckpt-32732.meta
-#forward=~/song/asr/bin/forward_skip_lstm_supervised.py 
+# skiprnn trained with supervised training
+dir=exp/hyperud/decode_bd_tgpr_dev93_skiprnn_sv_a4
+model=~/song/asr/skip_lstm_supervised_a7/best_model.ckpt-32732.meta
+~/song/asr/skiprnn_sv_a4/best_model.ckpt-9352.meta
+forward=~/song/asr/bin/forward_skiprnn_sv.py
 
 n_batch=1
 
