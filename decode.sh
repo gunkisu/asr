505593f564d2d485c03b69f85ee945ec2b08f5ce
Inchul Song <icsong@gmail.com>
2017-08-15 11:02:23 -0400

    Getting everything from the command line

--- decode.sh
+++ decode.sh
@@ -5,21 +5,25 @@ source /u/songinch/tensorflow/bin/activate
 cd ~/song/kaldi/egs/wsj/s5
 source ./path.sh
 
+
+dir=$1
+model=$2
+forward=$3
+
 # skiprnn trained by reinforce
-#dir=exp/hyperud/decode_bd_tgpr_dev93_skiprnn_ri_a6_y_e32
-#model=~/song/asr/skiprnn_ri_a6_y_e32/best_model.ckpt-11690.meta
+#dir=exp/hyperud/decode_bd_tgpr_dev93_skiprnn_ri_a6_fa20
+#model=~/song/asr/skiprnn_ri_a6_fa20/best_model.ckpt-21042.meta
 #forward=~/song/asr/bin/forward_skiprnn_ri.py
 
 # skiprnn with subsampling
-#dir=exp/hyperud/decode_bd_tgpr_dev93_skiprnn_subsample_s1
-#model=~/song/asr/skiprnn_subsample_s1/best_model.ckpt-7014.meta
+#dir=exp/hyperud/decode_bd_tgpr_dev93_skiprnn_subsample_s4
+#model=~/song/asr/skiprnn_subsample_s4/best_model.ckpt-28056.meta
 #forward=~/song/asr/bin/forward_skiprnn_subsample.py
 
 # skiprnn trained with supervised training
-dir=exp/hyperud/decode_bd_tgpr_dev93_skiprnn_sv_a4
-model=~/song/asr/skip_lstm_supervised_a7/best_model.ckpt-32732.meta
-~/song/asr/skiprnn_sv_a4/best_model.ckpt-9352.meta
-forward=~/song/asr/bin/forward_skiprnn_sv.py
+#dir=exp/hyperud/decode_bd_tgpr_dev93_skiprnn_sv_a8
+#model=~/song/asr/skiprnn_sv_a8/best_model.ckpt-30394.meta
+#forward=~/song/asr/bin/forward_skiprnn_sv.py
 
 n_batch=1
 
