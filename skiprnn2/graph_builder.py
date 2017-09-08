Stopping gradient from flowing by default; Bug fix

skiprnn2/graph_builder.py
@@ -8,10 +8,10 @@ import utils
 import tensorflow as tf 
 
 def stop_gradient(tensor, args):
-    if args.stop_gradient:
-        return tf.stop_gradient(tensor)
-    else:
+    if args.no_stop_gradient:
         return tensor
+    else:
+        return tf.stop_gradient(tensor)
 
 def lstm_state(n_hidden, layer, n_proj, backward=False):
     c_name = 'cstate_{}'.format(layer)