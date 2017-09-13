6ea13fc7a20d4014d769ff608f1a74d8dbff132f
Inchul Song <icsong@gmail.com>
2017-09-13 14:39:54 -0400

    Bug fix

--- decode_ted.sh
+++ decode_ted.sh
@@ -8,7 +8,11 @@ symbol_table=$recipe_dir/exp/tri3/graph/words.txt
 mdl=$recipe_dir/exp/hyperud/final.mdl
 hclg=$recipe_dir/exp/tri3/graph/HCLG.fst
 
-source $recipe_dir/path.sh
+datapath=~/song/data/speech/tedlium_fbank123.h5
+
+
+cd $recipe_dir
+source ./path.sh
 
 model=$1
 dir=$2
@@ -18,5 +22,5 @@ dataset=$4
 n_batch=1
 
 mkdir $dir
-python -u $forward --n-batch $n_batch --metafile $model --dataset $dataset --no-copy | latgen-faster-mapped --min-active=200 --max-active=7000 --max-mem=50000000 --beam=13.0 --lattice-beam=8.0 --acoustic-scale=0.1 --word-symbol-table=$symbol_table $mdl $hclg ark:- "ark:|gzip -c > $dir/lat.1.gz"
+python -u $forward --n-batch $n_batch --metafile $model --data-path $datapath --dataset $dataset --no-copy | latgen-faster-mapped --min-active=200 --max-active=7000 --max-mem=50000000 --beam=13.0 --lattice-beam=8.0 --acoustic-scale=0.1 --word-symbol-table=$symbol_table $mdl $hclg ark:- "ark:|gzip -c > $dir/lat.1.gz"
 
